d2a3d8281bd94bfb2d20ef19af087c55efb4dc63
Rojesh Prajapati <yomeronepal7@gmail.com>
2020-10-12 23:34:54 +0545

    Initial Commit

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+
+    path('',views.store,name='store'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('update_item/',views.updateItem,name='update_item'),
+    path('process_order/',views.processOrder,name='process_order')
+]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
